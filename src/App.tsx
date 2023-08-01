7d189728cac44cdcd126e5539abfe0d79f8c1c9a
AramZakaryan <aram.h.zakaryan@gmail.com>
2023-08-01 12:24:16 +0400

    Result of Experiments with useRef

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,45 @@
-import React from 'react';
+import React, {LegacyRef, MutableRefObject, useRef, useState} from 'react';
 
 const App = () => {
+
+    const [counter, setCounter] = useState(0)
+
+    const [name, setName] = useState<string>()
+    const [surname, setSurame] = useState<string>()
+    // console.log(name, surname)
+
+    const ref1 = useRef<any>()
+    const ref2 = useRef<any>()
+
     return (
         <div>
-            Hello
+                <input type="text"
+                       placeholder={"name"}
+                       value={name}
+                       onChange={e=>setName(e.currentTarget.value)}
+                       ref={ref1}
+
+                />
+                <br/>
+                <input type="text"
+                       placeholder={"surname"}
+                       value={surname}
+                       onChange={e=>setSurame(e.currentTarget.value)}
+                       ref={ref2}
+
+                />
+                <br/>
+                <button
+                    onClick={()=>{
+                        ref2.current.value="1"}}
+                >
+                    Change Focus
+                </button>
+
+
+
         </div>
     );
 }
-type MessageType = {
-    message: string
-}
 
-function HelloMessage(props: MessageType) {
-    return <h1>{props.message}</h1>
-}
-
-const ByeMessage: React.FC<MessageType> = (props) => {
-    return <h1>{props.message}</h1>
-}
 export default App;
